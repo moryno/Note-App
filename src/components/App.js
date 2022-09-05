bfd82531e9b934c33762a0459a7c854eda1de5a9
Maurice <mauricenganga41@gmail.com>
2022-09-05 07:15:48 +0300

    Bind the endpoint to the client side

--- src/components/App.js
+++ src/components/App.js
@@ -1,22 +1,33 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import CreateNotes from "./createNote/CreateNotes";
 import Footer from "./footer/Footer";
 import Navbar from "./navbar/Navbar";
 import Notes from "./notes/Notes";
+import noteApi from "../apis";
 
 function App() {
   const [notes, setNotes] = useState([]);
 
-  const addNote = (newNotes) => {
-    console.log(newNotes);
+  useEffect(() => {
+    const fetchNotes = async () => {
+      const { data } = await noteApi.get("/notes");
+      setNotes(data);
+    };
+    fetchNotes();
+  }, []);
+
+  const addNote = async (newNote) => {
+    const { data } = await noteApi.post("/notes", newNote);
     setNotes((prevNotes) => {
-      return [...prevNotes, newNotes];
+      return [...prevNotes, data];
     });
   };
-  const onDelete = (id) => {
+
+  const onDelete = async (id) => {
+    await noteApi.delete(`/notes/${id}`);
     setNotes((prevNotes) => {
-      return prevNotes.filter((note, index) => {
-        return index !== id;
+      return prevNotes.filter((noteItem) => {
+        return noteItem.id !== id;
       });
     });
   };
@@ -26,7 +37,7 @@ function App() {
       <Navbar />
       <CreateNotes onAdd={addNote} />
       {notes?.map((note) => {
-        return <Notes note={note} onDelete={onDelete} />;
+        return <Notes key={note.id} note={note} onDelete={onDelete} />;
       })}
 
       <Footer />
--- src/components/notes/Notes.js
+++ src/components/notes/Notes.js
@@ -2,15 +2,15 @@ import React from "react";
 import "./Notes.css";
 
 function Card({ note, onDelete }) {
-  function handleClick() {
+  const handleDelete = () => {
     onDelete(note.id);
-  }
+  };
 
   return (
     <div className="card">
       <h1>{note?.title}</h1>
       <p>{note?.content}</p>
-      <button onClick={handleClick}>
+      <button onClick={handleDelete}>
         <i class="fa-solid fa-trash-can"></i>
       </button>
     </div>
